642bddad3d155878d4ad77386eccd3e8a9dd32ff
root <root@srv1642489.hstgr.cloud>
2026-07-30 20:29:59 +0000

    Fix PWA icon links

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get pages_home_url
     assert_response :success
   end
+
+  test "pwa manifest references existing icon assets" do
+    get "/manifest.json"
+
+    assert_response :success
+    assert_includes response.body, "/icon-192.png"
+    assert_includes response.body, "/icon-512.png"
+    assert_no_match %r{"/icon\.png"}, response.body
+  end
 end
