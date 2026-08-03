1b1995485a8cc61a71d0f6f3d332e33a8da18464
root <root@srv1642489.hstgr.cloud>
2026-08-03 16:08:41 +0000

    Update browser title and favicon

--- test/controllers/admin/localization_controller_test.rb
+++ test/controllers/admin/localization_controller_test.rb
@@ -9,7 +9,7 @@ class Admin::LocalizationControllerTest < ActionDispatch::IntegrationTest
     get admin_root_path
 
     assert_response :success
-    assert_includes response.body, "Admin-Übersicht"
+    assert_select "title", "DB × DREI"
     assert_includes response.body, "Benutzer insgesamt"
     assert_includes response.body, 'data-controller="turbo-progress modal-manager"'
     assert_no_match(/Welcome|Total Users|Pending Invites/, response.body)
@@ -19,7 +19,7 @@ class Admin::LocalizationControllerTest < ActionDispatch::IntegrationTest
     get admin_users_path
 
     assert_response :success
-    assert_includes response.body, "Benutzerrechte"
+    assert_select "title", "DB × DREI"
     assert_includes response.body, "Es gibt nur zwei Rollen"
     assert_includes response.body, "Letzte Anmeldung"
     assert_no_match(/Create user|Back to Dashboard|Last Login/, response.body)
@@ -29,6 +29,7 @@ class Admin::LocalizationControllerTest < ActionDispatch::IntegrationTest
     get admin_invites_path
 
     assert_response :success
+    assert_select "title", "DB × DREI"
     assert_includes response.body, "Benutzer einladen"
     assert_includes response.body, "Einladung senden"
     assert_no_match(/Invite user|Send invite|Back to Dashboard/, response.body)
