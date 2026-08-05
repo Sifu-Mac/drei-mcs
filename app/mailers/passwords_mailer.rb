8517964a3f54cba29612b1625dca858c2f7c6adf
root <root@srv1642489.hstgr.cloud>
2026-08-05 11:38:42 +0000

    Localize DB x DREI emails

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,6 +1,6 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
     @user = user
-    mail subject: "Reset your password", to: user.email_address, content_type: "text/plain"
+    mail subject: "Passwort zurücksetzen – DB × DREI", to: user.email_address, content_type: "text/plain"
   end
 end
--- test/mailers/passwords_mailer_test.rb
+++ test/mailers/passwords_mailer_test.rb
@@ -5,10 +5,13 @@ class PasswordsMailerTest < ActionMailer::TestCase
     user = users(:one)
     mail = PasswordsMailer.reset(user)
 
-    assert_equal "Reset your password", mail.subject
+    assert_equal "Passwort zurücksetzen – DB × DREI", mail.subject
     assert_equal [ user.email_address ], mail.to
-    assert_equal [ "hello@digitalbackup.at" ], mail.from
+    assert_equal [ "noreply@digitalbackup.at" ], mail.from
+    assert_equal [ "DB × DREI" ], mail[:from].display_names
     assert_match %r{\Ahttps://example\.com/passwords/}, extract_reset_url(mail)
+    assert_includes mail.body.decoded, "Passworts für DB × DREI"
+    assert_includes mail.body.decoded, "Digitalbackup MW GmbH"
   end
 
   private
